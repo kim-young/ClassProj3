fe9798ed7ac4f3d5f3ce259c1438737da2ceb0f2
kim-young <younghyun87@gmail.com>
2018-05-24 19:39:06 -0400

    C# project 3

---
+++ ClassProj3/Calculation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ClassProj3
+{
+    public class Calculation
+    {
+        public Calculation()
+        {
+        }
+
+  //      public static int MathOperation(int numberOne)
+		//{
+		//	int result = numberOne + 100;
+		//	return result;        
+		//}
+
+        public static int MathOperation(int numberOne, params int[] numberTwo)
+		{
+			int result = numberOne;
+
+			if (numberTwo != null)
+			{
+				foreach (var i in numberTwo)
+				{
+					result += i;
+				}
+			}         
+			return result;
+		}
+    }
+}
---
+++ ClassProj3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassProj3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input first number.");
+			int numberOne = int.Parse(Console.ReadLine());
+            
+			Console.WriteLine("Input second number. (optional)");
+			if (Console.ReadLine() != null)
+			{
+				int numberTwo = int.Parse(Console.ReadLine());
+				Console.WriteLine(Calculation.MathOperation(numberOne, numberTwo));
+			}
+
+			else
+			{
+				int numberTwo = 0;
+				Console.WriteLine(Calculation.MathOperation(numberOne, numberTwo));            
+			}
+        }
+    }
+}
